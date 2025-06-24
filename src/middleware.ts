5498bbb4731fb2b401ede2cb782e73b21d46743d
jyooni99 <berry3891@naver.com>
2025-06-25 00:15:33 +0900

    feat: studio를 protected Route로 설정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/protected(.*)"]); //protected로 시작하는 모든 경로를 로그인 필요 경로로 설정
+const isProtectedRoute = createRouteMatcher(["/studio(.*)"]); //studio로 시작하는 모든 경로를 로그인 필요 경로로 설정
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect(); // 로그인된 사용자만 요청을 진행, 로그인 하지 않은 경우 로그인 페이지로 리디렉션
