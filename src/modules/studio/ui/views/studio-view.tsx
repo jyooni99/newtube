da2b58710eea208ed46683bf2397a3035730d6a1
jyooni <berry3891@naver.com>
2025-06-30 22:04:53 +0900

    chore: view -> views로 폴더명 변경

--- src/modules/studio/ui/views/studio-view.tsx
+++ src/modules/studio/ui/views/studio-view.tsx
@@ -2,7 +2,7 @@ import VideosSection from "@/modules/studio/ui/sections/videos-section";
 
 function StudioView() {
   return (
-    <div className="flex flex-col gap-y-6 pt-2.5">
+    <div className="flex flex-col gap-y-6 pt-2.5 w-full">
       <div className="px-4">
         <h1 className="text-2xl font-bold">Channel content</h1>
         <p className="text-xs text-muted-foreground">
